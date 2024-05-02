6bde5b2c8c9db2291e4ab1c1aa46e0e1a5e35935
midu-zack <midlajmidu0002@gmail.com>
2024-05-02 14:12:51 +0530

    database changed (compass convert to atlas)

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const exphbs = require("express-handlebars");
 const express = require("express");
-const path = require('path')
-const dotenv = require("dotenv")
-dotenv.config()
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const userRouter =require("./routers/user");
-const authRoutes =require("./routers/auth")
-const adminRouter =require("./routers/admin");
-const categorieRouter =require("./routers/categerie")
-const productRouter =require("./routers/product")
-const customersRouter =require("./routers/customers")
-const cartRouter =require("./routers/cart")
-const wishlistRouter = require("./routers/wishlist")
-const checkoutRouter = require("./routers/checkout")
-
- 
-
+const userRouter = require("./routers/user");
+const authRoutes = require("./routers/auth");
+const adminRouter = require("./routers/admin");
+const categorieRouter = require("./routers/categerie");
+const productRouter = require("./routers/product");
+const customersRouter = require("./routers/customers");
+const cartRouter = require("./routers/cart");
+const wishlistRouter = require("./routers/wishlist");
+const checkoutRouter = require("./routers/checkout");
 
 const bodyParser = require("body-parser");
 const connectDatabase = require("./config/database");
 // const { session } = require("passport");
-const session = require("express-session")
-const cookieParser = require('cookie-parser');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cookieParser());
@@ -33,16 +30,12 @@ const handlebars = exphbs.create({
     stringify: function (context) {
       return JSON.stringify(context);
     },
-
   },
 });
 
 // Configure Handlebars
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
-
-
-
 
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -51,32 +44,30 @@ app.use(express.static(path.join(__dirname, "views")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(session({
-  secret:"your-secret-key",
+app.use(
+  session({
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: false,
     cookie: {
       secure: false, // Set to true in production if using HTTPS
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
-    } 
-}))
-
+    },
+  })
+);
 
 app.use("/", authRoutes);
 app.use("/", userRouter);
 app.use("/", adminRouter);
-app.use("/", categorieRouter)
-app.use("/", productRouter)
-app.use("/",customersRouter)
-app.use("/",cartRouter)
-app.use("/",wishlistRouter)
-app.use("/",checkoutRouter)
+app.use("/", categorieRouter);
+app.use("/", productRouter);
+app.use("/", customersRouter);
+app.use("/", cartRouter);
+app.use("/", wishlistRouter);
+app.use("/", checkoutRouter);
 
-
-
-app.listen(port,async() => {
+app.listen(port, async () => {
   console.log(`server running on  ${port}`);
-  await connectDatabase() 
+  await connectDatabase();
 });
