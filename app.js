54fccdb8210db55553f7455abae2e83f636ff904
miduzack <miduzack001@gmail.com>
2024-09-15 22:46:39 +0530

    latest

--- app.js
+++ app.js
@@ -1,3 +1,94 @@
+// const exphbs = require("express-handlebars");
+// const express = require("express");
+// const path = require("path");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const userRouter = require("./routers/user");
+// const authRoutes = require("./routers/auth");
+// const adminRouter = require("./routers/admin");
+// const categorieRouter = require("./routers/categerie");
+// const productRouter = require("./routers/product");
+// const customersRouter = require("./routers/customers");
+// const cartRouter = require("./routers/cart");
+// const wishlistRouter = require("./routers/wishlist");
+// const checkoutRouter = require("./routers/checkout");
+
+// const bodyParser = require("body-parser");
+// const connectDatabase = require("./config/database");
+// // const { session } = require("passport");
+// const session = require("express-session");
+// const cookieParser = require('cookie-parser');
+
+
+ 
+
+// const app = express();
+// app.use(cookieParser());
+ 
+// const port = 2328;
+
+ 
+
+
+
+// // Register a custom helper function for JSON.stringify
+// const handlebars = exphbs.create({
+//   helpers: {
+//     stringify: function (context) {
+//       return JSON.stringify(context);
+//     },
+//   },
+// });
+
+// // Configure Handlebars
+// app.engine("handlebars", handlebars.engine);
+// app.set("view engine", "handlebars");
+
+// app.set("view engine", "hbs");
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "views")));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(
+//   session({
+//     secret: "your-secret-key",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       secure: false, // Set to true in production if using HTTPS
+//       httpOnly: true,
+//       maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+//     },
+//   })
+// );
+
+// app.use("/", authRoutes);
+// app.use("/", userRouter);
+// app.use("/", adminRouter);
+// app.use("/", categorieRouter);
+// app.use("/", productRouter);
+// app.use("/", customersRouter);
+// app.use("/", cartRouter);
+// app.use("/", wishlistRouter);
+// app.use("/", checkoutRouter);
+
+// app.listen(port, async () => {
+//   console.log(`server running on  ${port}`);
+//   await connectDatabase();
+// });
+
+
+
+
+
+
+
+
+
+// uUPDATE VERSION
 const exphbs = require("express-handlebars");
 const express = require("express");
 const path = require("path");
@@ -16,21 +107,13 @@ const checkoutRouter = require("./routers/checkout");
 
 const bodyParser = require("body-parser");
 const connectDatabase = require("./config/database");
-// const { session } = require("passport");
 const session = require("express-session");
-const cookieParser = require('cookie-parser');
-
-
- 
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cookieParser());
- 
+
 const port = 2328;
-
- 
-
-
 
 // Register a custom helper function for JSON.stringify
 const handlebars = exphbs.create({
@@ -41,13 +124,12 @@ const handlebars = exphbs.create({
   },
 });
 
-// Configure Handlebars
+// Configure Handlebars (only express-handlebars)
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
-app.set("view engine", "hbs");
+// Set static file directory
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "views")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -65,6 +147,7 @@ app.use(
   })
 );
 
+// Routers
 app.use("/", authRoutes);
 app.use("/", userRouter);
 app.use("/", adminRouter);
@@ -75,7 +158,8 @@ app.use("/", cartRouter);
 app.use("/", wishlistRouter);
 app.use("/", checkoutRouter);
 
+// Start server and connect to database
 app.listen(port, async () => {
-  console.log(`server running on  ${port}`);
+  console.log(`Server running on port ${port}`);
   await connectDatabase();
 });
