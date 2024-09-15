835a257ea724f8ed7702b3b56b5a18284233bf9b
miduzack <miduzack001@gmail.com>
2024-09-15 23:42:15 +0530

    latest

--- app.js
+++ app.js
@@ -1,97 +1,3 @@
-// const exphbs = require("express-handlebars");
-// const express = require("express");
-// const path = require("path");
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const userRouter = require("./routers/user");
-// const authRoutes = require("./routers/auth");
-// const adminRouter = require("./routers/admin");
-// const categorieRouter = require("./routers/categerie");
-// const productRouter = require("./routers/product");
-// const customersRouter = require("./routers/customers");
-// const cartRouter = require("./routers/cart");
-// const wishlistRouter = require("./routers/wishlist");
-// const checkoutRouter = require("./routers/checkout");
-
-// const bodyParser = require("body-parser");
-// const connectDatabase = require("./config/database");
-// // const { session } = require("passport");
-// const session = require("express-session");
-// const cookieParser = require('cookie-parser');
-
-
- 
-
-// const app = express();
-// app.use(cookieParser());
- 
-// const port = 2328;
-
- 
-
-
-
-// // Register a custom helper function for JSON.stringify
-// const handlebars = exphbs.create({
-//   helpers: {
-//     stringify: function (context) {
-//       return JSON.stringify(context);
-//     },
-//   },
-// });
-
-// // Configure Handlebars
-// app.engine("handlebars", handlebars.engine);
-// app.set("view engine", "handlebars");
-
-// app.set("view engine", "hbs");
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.static(path.join(__dirname, "views")));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   session({
-//     secret: "your-secret-key",
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       secure: false, // Set to true in production if using HTTPS
-//       httpOnly: true,
-//       maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
-//     },
-//   })
-// );
-
-// app.use("/", authRoutes);
-// app.use("/", userRouter);
-// app.use("/", adminRouter);
-// app.use("/", categorieRouter);
-// app.use("/", productRouter);
-// app.use("/", customersRouter);
-// app.use("/", cartRouter);
-// app.use("/", wishlistRouter);
-// app.use("/", checkoutRouter);
-
-// app.listen(port, async () => {
-//   console.log(`server running on  ${port}`);
-//   await connectDatabase();
-// });
-
-
-
-
-
-
-
-
-
-// uUPDATE VERSION
-
-const MongoStore = require('connect-mongo');
-
 const exphbs = require("express-handlebars");
 const express = require("express");
 const path = require("path");
@@ -110,13 +16,21 @@ const checkoutRouter = require("./routers/checkout");
 
 const bodyParser = require("body-parser");
 const connectDatabase = require("./config/database");
+// const { session } = require("passport");
 const session = require("express-session");
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
+
+
+ 
 
 const app = express();
 app.use(cookieParser());
-
+ 
 const port = 2328;
+
+ 
+
+
 
 // Register a custom helper function for JSON.stringify
 const handlebars = exphbs.create({
@@ -127,33 +41,30 @@ const handlebars = exphbs.create({
   },
 });
 
-// Configure Handlebars (only express-handlebars)
+// Configure Handlebars
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 
-// Set static file directory
+app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: 'your-secret-key',
+    secret: "your-secret-key",
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URI,
-    }),
     cookie: {
-      secure: false, // Set to true in production with HTTPS
+      secure: false, // Set to true in production if using HTTPS
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
     },
   })
 );
 
-// Routers
 app.use("/", authRoutes);
 app.use("/", userRouter);
 app.use("/", adminRouter);
@@ -164,8 +75,12 @@ app.use("/", cartRouter);
 app.use("/", wishlistRouter);
 app.use("/", checkoutRouter);
 
-// Start server and connect to database
 app.listen(port, async () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`server running on  ${port}`);
   await connectDatabase();
 });
+
+
+
+
+ 
